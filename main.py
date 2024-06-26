cb544cc7b4f49f03a54524bc04d7abb5fd465f45
E Vishnu prasad <109452822+VSeekk@users.noreply.github.com>
2024-06-26 18:34:42 +0530

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
+
 response= requests.get(URL)
 movie_webpage= response.text
 soup= BeautifulSoup(movie_webpage, "html.parser")
